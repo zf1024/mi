558c2bbfac2f4379741ba50d5caae9fc992d71a0
zf1024 <626205766@qq.com>
2017-12-13 22:34:10 +0800

    圆圈点播

--- git_zf_xiaomi/js/Carousel figure.js
+++ git_zf_xiaomi/js/Carousel figure.js
@@ -1,27 +1,24 @@
 //下一页
 var arrs = document.querySelectorAll("nav a");
 var Lis = document.querySelectorAll("ul li");
+var circles = document.querySelectorAll("ol li");
 var flag = 0;
 //手动向右轮播
 arrs[1].addEventListener("click", function() {
+	Lis[flag].className = "";
 	flag++;
 	if(flag == Lis.length) {
 		flag = 0;
-		for(var i = 0; i < Lis.length; i++) {
-			Lis[i].className = "";
-		}
 	}
 	Lis[flag].className = "now";
 })
 //手动向左轮播
 arrs[0].addEventListener("click", function() {
+	Lis[flag].className = "";
 	flag--;
 	if(flag < 0) {
 		flag = Lis.length - 1;
-		for(var i = 0; i < Lis.length; i++) {
-			Lis[i].className = "";
-		}
 	}
 	Lis[flag].className = "now";
 })
-//遍历所有图片
+//点击圆圈轮播
