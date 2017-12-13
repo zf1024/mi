0b376be8ae123c20248bb28e9328552311fafde5
zf1024 <626205766@qq.com>
2017-12-13 20:27:25 +0800

    z-index问题

--- git_zf_xiaomi/js/Carousel figure.js
+++ git_zf_xiaomi/js/Carousel figure.js
@@ -1,8 +1,27 @@
 //下一页
 var arrs = document.querySelectorAll("nav a");
 var Lis = document.querySelectorAll("ul li");
-arrs[0].onclick = function() {
-	for(i = 0; i <= arrs.length; i++) {
-
+var flag = 0;
+//手动向右轮播
+arrs[1].addEventListener("click", function() {
+	flag++;
+	if(flag == Lis.length) {
+		flag = 0;
+		for(var i = 0; i < Lis.length; i++) {
+			Lis[i].className = "";
+		}
 	}
-}
+	Lis[flag].className = "now";
+})
+//手动向左轮播
+arrs[0].addEventListener("click", function() {
+	flag--;
+	if(flag < 0) {
+		flag = Lis.length - 1;
+		for(var i = 0; i < Lis.length; i++) {
+			Lis[i].className = "";
+		}
+	}
+	Lis[flag].className = "now";
+})
+//遍历所有图片
